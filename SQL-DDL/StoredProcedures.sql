1a8fc757275ab56ab4298cfcf71d9f1c690d09ad
mdemet01 <mdemet01@ucy.ac.cy>
2023-11-24 11:15:39 +0200

    Fixed stored procedure for getProperties
    Co-Authored-By: ChrisZachariadis <103041867+ChrisZachariadis@users.noreply.github.com>

--- SQL-DDL/StoredProcedures.sql
+++ SQL-DDL/StoredProcedures.sql
@@ -1,10 +1,14 @@
 CREATE PROCEDURE getProperties
-@Property_Location varchar(20),
+@Property_Location VARCHAR(20)
 AS
 BEGIN
-IF NOT EXISTS (SELECT * FROM [dbo].PROPERTY WHERE [dbo].Property_Location = @Property_Location) BEGIN PRINT 'Unavailable location' RETURN END
-
+IF NOT EXISTS (
+SELECT * 
+FROM [dbo].[PROPERTY]
+WHERE [Property_Location] = @Property_Location
+) BEGIN PRINT 'Unavailable location' RETURN 
+END
 SELECT * 
 FROM [dbo].PROPERTY 
-WHERE [dbo].Property_Location = @Property_Location
+WHERE [Property_Location] = @Property_Location
 END
