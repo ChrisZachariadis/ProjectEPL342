2b07e5674cc929e7d5f5d08c14afd90d243aa19d
mdemet01 <mdemet01@ucy.ac.cy>
2023-11-24 11:50:13 +0200

    fixed stored procedure for getProperties
    Co-Authored-By: ChrisZachariadis <103041867+ChrisZachariadis@users.noreply.github.com>

--- SQL-DDL/StoredProcedures.sql
+++ SQL-DDL/StoredProcedures.sql
@@ -1,4 +1,6 @@
-CREATE PROCEDURE getProperties
+--get products based on location
+--returns product ID, property name, product price and room type.
+CREATE PROCEDURE getProduct
 @Property_Location VARCHAR(20)
 AS
 BEGIN
@@ -8,7 +10,21 @@ FROM [dbo].[PROPERTY]
 WHERE [Property_Location] = @Property_Location
 ) BEGIN PRINT 'Unavailable location' RETURN 
 END
-SELECT * 
-FROM [dbo].PROPERTY 
-WHERE [Property_Location] = @Property_Location
+SELECT B.[Product_ID], A.[Property_Name],B.[Product_Price], C.[Bed_Type]
+FROM [dbo].[PROPERTY] A , [dbo].[PRODUCT] B , [dbo].[ROOM_TYPE] C
+WHERE A.[Property_Location] = @Property_Location AND A.[Property_ID] = B.[Property_ID] AND  B.[Room_Type_ID]=C.[Room_Type_ID]
 END
+
+--get Amenities of a product based on Product_ID 
+
+
+
+
+
+
+
+--getProducts based on property id
+--getAvailableProducts based on date and property id
+--getProducts based on property id and facilities
+--getProducts based on property id and facilities and amenities
+--getProperties based on Property_type
