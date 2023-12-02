dde6dbbe9680cf9faef265f37e2ada071905eb4f
Christos Zachariadis <czacha02@ucy.ac.cy>
2023-12-02 17:34:27 +0200

    needs work

--- SQL-DDL/ReportsSPs.sql
+++ SQL-DDL/ReportsSPs.sql
@@ -1,269 +1,3 @@
--- CREATE PROCEDURE spRegister_User
---     -- @User_ID INT,
---     @Date_of_Birth DATE,
---     @User_Type VARCHAR(15),
---     @First_Name VARCHAR(15),
---     @Last_Name VARCHAR(15),
---     @Email VARCHAR(50),
---     @Passwd VARCHAR(20),
---     @Gender CHAR(1)
--- AS
--- BEGIN
---     SET @First_Name = RTRIM(@First_Name);
---     SET @Last_Name = RTRIM(@Last_Name);
---     SET @Email = RTRIM(@Email);
---     SET @Passwd = RTRIM(@Passwd);
---     SET @User_Type = RTRIM(@User_Type);
-
---     DECLARE @Approved CHAR(1);
---     IF @User_Type = 'Customer' 
---     BEGIN
---         SET @Approved = 'Y';
---     END
---     ELSE IF @User_Type = 'Property_Owner'
---     BEGIN
---         SET @Approved = 'N';
---     END
---     -- Check if the Email format is valid
---     IF @Email NOT LIKE '%@%.%'
---     BEGIN
---         RAISERROR ('Invalid Email format', 16, 1);
---         RETURN;
---     END
---     ELSE IF EXISTS (
---         SELECT *
---         FROM [dbo].[USER]
---         WHERE [Email] = @Email
---     )
---     BEGIN 
---         PRINT 'Error: Email already exists';
---     END
---     ELSE
---     BEGIN
---         -- Insert user data into the table with dynamic User_Type
---         INSERT INTO [dbo].[USER] 
---             (Date_of_Birth, User_Type, First_Name, Last_Name, Email, Passwd, Gender, Approved)
---         VALUES 
---             (@Date_of_Birth, @User_Type, @First_Name, @Last_Name, @Email, @Passwd, @Gender, @Approved);
---     END
--- END;
-
-
--- GO
-
--- ------------------USER LOGIN--------------------------------
-
--- CREATE PROCEDURE spLOGIN 
---     @Email VARCHAR(50),
---     @Passwd VARCHAR(20) 
--- AS 
--- BEGIN
---     SET @Email = LTRIM(RTRIM(@Email));
---     SET @Passwd = LTRIM(RTRIM(@Passwd));
-
---     -- Check if the credentials exist in the database
---     IF NOT EXISTS (
---         SELECT *
---         FROM [dbo].[USER]
---         WHERE [Email] = @Email
---           AND [Passwd] = @Passwd
---     )
---     BEGIN 
---         PRINT 'Error: Invalid Email or password';
---         -- It's generally not a good practice to print the hash of the password.
---         -- The below line can be commented out or removed if not required.
---         -- PRINT HASHBYTES('SHA2_256', @Passwd);
---     END
---     ELSE
---     BEGIN
---         -- Also return the user_id of the user who logged in
---         SELECT [user_id],[User_Type],[Approved]
---         FROM [dbo].[USER]
---         WHERE [Email] = @Email
---         AND [Passwd] = @Passwd;
---     END
--- END;
-
--- GO
-
--- CREATE PROCEDURE spGetUserType
---     @User_ID INT
--- AS
--- BEGIN
---     -- Select User_Type for the given user_id
---     SELECT User_Type
---     FROM [dbo].[USER]
---     WHERE user_id = @User_ID;
--- END;
-
-
--------ADMIN DASHBOARD // CAN ADD AND EDIT PRODUCTS USING PRODUCT_ID.--------
-
---Add a product with a new product_ID.
--- CREATE PROCEDURE spInsert_Product
---     @Product_ID INT,
---     @Product_Price DECIMAL(10, 2),
---     @Max_Guests INT,
---     @Product_Description NVARCHAR(MAX),
---     @Room_Type_ID INT,
---     @Property_ID INT
--- AS
--- BEGIN
-
---     INSERT INTO [dbo].[PRODUCT] (Product_ID,Product_Price, Max_Guests, Product_Description, Room_Type_ID, Property_ID)
---     VALUES (@Product_ID, @Product_Price, @Max_Guests, @Product_Description, @Room_Type_ID, @Property_ID);
--- END
-
-
-
--- GO
--- --Edit the product based on product_ID
--- CREATE PROCEDURE spEdit_Product
---     @Product_ID INT,
---     @Product_Price DECIMAL(10, 2),
---     @Max_Guests INT,
---     @Product_Description NVARCHAR(MAX),
---     @Room_Type_ID INT,
---     @Property_ID INT
--- AS
--- BEGIN
---     UPDATE [dbo].[PRODUCT]
---     SET Product_Price = @Product_Price,
---         Max_Guests = @Max_Guests,
---         Product_Description = @Product_Description,
---         Room_Type_ID = @Room_Type_ID,
---         Property_ID = @Property_ID
---     WHERE Product_ID = @Product_ID;
--- END
-
--- GO
-
-
--- PROPERTY MANAGER // CAN ADD PROPERTY AND EDIT THE AVAILABILITY / PRICE  -- AN ADMIN NEEDS TO APPROVE HIS CHANGES 
-
-
--- GET properties that belong to the property owner with the given user_id. (property owner) 
-
--- CREATE PROCEDURE spGetPropertyOwnerProperties
---     @User_ID INT
--- AS
--- BEGIN
---     -- Select properties that belong to the user
---     SELECT Property_ID, Property_Name, Property_Address, Property_Description, 
---            Property_Coordinates, Property_Location, Owner_ID, Owner_First_Name, 
---            Owner_Last_Name, Property_Type_ID
---     FROM [dbo].[PROPERTY]
---     WHERE User_ID = @User_ID;
--- END;
-
--- GO
-
-
--- CREATE PROCEDURE spInsert_Property
---     @Property_ID INT,  
---     @Property_Name VARCHAR(50),
---     @Property_Address VARCHAR(50),
---     @Property_Description VARCHAR(15),
---     @Property_Coordinates VARCHAR(20),
---     @Property_Location VARCHAR(20),
---     @Owner_ID INT,
---     @Owner_First_Name VARCHAR(15),
---     @Owner_Last_Name VARCHAR(15),
---     @Property_Type_ID INT,
---     @User_ID INT
--- AS
--- BEGIN
---     INSERT INTO [dbo].[PROPERTY] (
---         Property_ID, Property_Name, Property_Address, Property_Description, Property_Coordinates, Property_Location,
---         Owner_ID, Owner_First_Name, Owner_Last_Name, Property_Type_ID, User_ID
---     )
---     VALUES (
---         @Property_ID, @Property_Name, @Property_Address, @Property_Description, @Property_Coordinates,
---         @Property_Location, @Owner_ID, @Owner_First_Name, @Owner_Last_Name, @Property_Type_ID, @User_ID
---     );
--- END;
-
-
--- GO
--- EDIT PROPERTY BASED ON THE PROPERTY_ID 
-
--- CREATE PROCEDURE spUpdate_Property
---     @Property_ID INT,
---     @Property_Name VARCHAR(50),
---     @Property_Address VARCHAR(50),
---     @Property_Description VARCHAR(15),
---     @Property_Coordinates VARCHAR(20),
---     @Property_Location VARCHAR(20),
---     @Owner_ID INT,
---     @Owner_First_Name VARCHAR(15),
---     @Owner_Last_Name VARCHAR(15),
---     @Property_Type_ID INT,
---     @User_ID INT
--- AS
--- BEGIN
---     UPDATE [dbo].[PROPERTY]
---     SET 
---         Property_Name = @Property_Name,
---         Property_Address = @Property_Address,
---         Property_Description = @Property_Description,
---         Property_Coordinates = @Property_Coordinates,
---         Property_Location = @Property_Location,
---         Owner_ID = @Owner_ID,
---         Owner_First_Name = @Owner_First_Name,
---         Owner_Last_Name = @Owner_Last_Name,
---         Property_Type_ID = @Property_Type_ID,
---         User_ID = @User_ID
---     WHERE Property_ID = @Property_ID;
--- END;
-
-
-
---- FOR OWNER --- VIEW THE REGISTERED PROPERTY OWNERS AND IF HE WANT HE CAN APPROVE THEM.
--- GO
-
--- CREATE PROCEDURE spViewUnapprovedPropertyOwners
--- AS
--- BEGIN
---     SELECT User_ID, Date_of_Birth, First_Name, Last_Name, Email, Approved
---     FROM [dbo].[USER]
---     WHERE User_Type = 'Property Owner' AND Approved = 'N'
--- END;
-
-
--- GO
-
--- FOR OWNER --- APPROVE THE PROPERTY OWNER BASED ON THE USER_ID
-
--- CREATE PROCEDURE spApproveUnapprovedOwnersByID
---     @User_ID INT
--- AS
--- BEGIN
---     UPDATE [dbo].[USER]
---     SET Approved = 'Y'
---     WHERE User_ID = @User_ID
--- END;
-
--- GO
-
--- View reservations --- FOR OWNER --- Edit reservations.
-
-
-
--- CREATE PROCEDURE spViewReservations
---     @Product_ID INT,
---     @User_ID INT
--- AS
--- BEGIN
---     SELECT Reservation_ID, Reservation_Date, Review_ID, User_ID, Product_ID
---     FROM [dbo].[Reservations]
---     WHERE Product_ID = @Product_ID AND User_ID = @User_ID
--- END;
-
--- GO
-
-
------- THE ABOVE ARE DUPLICATES-- CAN BE REMOVED IF NOT NECESSARY ------------------------------
-
 ---------------------------------------------------------------------------------------
      --                             REPORTS                                      --
 ---------------------------------------------------------------------------------------
@@ -736,62 +470,9 @@ BEGIN
 END
 GO
 
+-- MAYBE WORKING ?? NEED TO CHECK LOGIC AGAIN 
+
 -- Stored procedure that returns the highest and lowest rated properties
-
--- CREATE PROCEDURE IdentifyPropertiesByRating
--- AS
--- BEGIN
---     -- Temporary table to store average ratings for each property
---     CREATE TABLE #PropertyRatings (
---         Property_ID INT,
---         Property_Name NVARCHAR(255),
---         AverageRating DECIMAL(5, 2)
---     );
-
---     -- Calculate average ratings for each property
---     INSERT INTO #PropertyRatings (Property_ID, Property_Name, AverageRating)
---     SELECT 
---         P.Property_ID, 
---         P.Property_Name, 
---         AVG(R.Review_Rating) AS AverageRating
---     FROM 
---         PROPERTY P
---     LEFT JOIN 
---         PRODUCT PR ON P.Property_ID = PR.Property_ID
---     LEFT JOIN 
---         RESERVATIONS RV ON PR.Product_ID = RV.Product_ID
---     LEFT JOIN 
---         REVIEWS R ON RV.Review_ID = R.Review_ID
---     WHERE 
---         R.Review_ID IS NOT NULL
---     GROUP BY 
---         P.Property_ID, 
---         P.Property_Name;
-
---     -- Select properties with the highest ratings
---     SELECT TOP 1 WITH TIES 
---         Property_ID, 
---         Property_Name, 
---         AverageRating
---     FROM 
---         #PropertyRatings
---     ORDER BY 
---         AverageRating DESC;
-
---     -- Select properties with the lowest ratings
---     SELECT TOP 1 WITH TIES 
---         Property_ID, 
---         Property_Name, 
---         AverageRating
---     FROM 
---         #PropertyRatings
---     ORDER BY 
---         AverageRating;
-
---     -- Cleanup
---     DROP TABLE #PropertyRatings;
--- END
-
 
 CREATE PROCEDURE IdentifyPropertiesByRating
 AS
@@ -897,14 +578,15 @@ GO
 -- it also returns rooms that where NEVER BOOKED IN THE SPECIFIC PERIOD AS "NEVER BOOKED"
 
 CREATE PROCEDURE GetPropertyRoomBookingStatus
-    @PropertyName NVARCHAR(255),
+    @PropertyTypeName NVARCHAR(255),
     @StartDate DATE,
     @EndDate DATE
 AS
-
+BEGIN
+    -- Check for 'empty' input and set to NULL
     IF (@PropertyTypeName = 'empty')
         SET @PropertyTypeName = NULL;
-BEGIN
+
     -- Select rooms that were booked every day within the specified period
     SELECT 
         PR.Product_ID, 
@@ -912,16 +594,16 @@ BEGIN
         'Fully Booked' AS BookingStatus
     FROM 
         PRODUCT PR
-    INNER JOIN
-        PROPERTY P ON PR.Property_ID = P.Property_ID
+    INNER JOIN PROPERTY P ON PR.Property_ID = P.Property_ID
+    INNER JOIN PROPERTY_TYPE PT ON P.Property_Type_ID = PT.Property_Type_ID
     WHERE 
-        P.Property_Name = @PropertyName AND
+        PT.Property_Type_Name = @PropertyTypeName AND
         NOT EXISTS (
-            SELECT 1
+            SELECT DISTINCT S.Stock_Date
             FROM STOCK S
             WHERE S.Product_ID = PR.Product_ID AND S.Stock_Date BETWEEN @StartDate AND @EndDate
             EXCEPT
-            SELECT 1
+            SELECT R.Reservation_Date
             FROM RESERVATIONS R
             WHERE R.Product_ID = PR.Product_ID AND R.Reservation_Date BETWEEN @StartDate AND @EndDate
         )
@@ -935,66 +617,51 @@ BEGIN
         'Never Booked' AS BookingStatus
     FROM 
         PRODUCT PR
-    INNER JOIN
-        PROPERTY P ON PR.Property_ID = P.Property_ID
+    INNER JOIN PROPERTY P ON PR.Property_ID = P.Property_ID
+    INNER JOIN PROPERTY_TYPE PT ON P.Property_Type_ID = PT.Property_Type_ID
     WHERE 
-        P.Property_Name = @PropertyName AND
+        PT.Property_Type_Name = @PropertyTypeName AND
         NOT EXISTS (
             SELECT 1
             FROM RESERVATIONS R
             WHERE R.Product_ID = PR.Product_ID AND R.Reservation_Date BETWEEN @StartDate AND @EndDate
         )
 END
+
+
+
 GO
 
 -- Stored procedure for generating a report of all rooms in a specific property that had at least ONE BOOKING EACH MONTH of a given calendar year
 -- cannot have null property id (diladi na tiponei gia ola ta ids)
 
 CREATE PROCEDURE GetRoomsWithMonthlyBookings
-    @PropertyName NVARCHAR(255),
+    @PropertyTypeName NVARCHAR(255),
     @Year INT
 AS
 BEGIN
-    -- Table to store rooms with at least one booking each month
-    CREATE TABLE #RoomsMonthlyBooked (
-        Product_ID INT,
-        RoomBookedEveryMonth BIT
-    );
+    IF (@PropertyTypeName = 'empty')
+        SET @PropertyTypeName = NULL;
 
-    -- Initialize the table with all rooms set to 'booked every month' (1)
-    INSERT INTO #RoomsMonthlyBooked (Product_ID, RoomBookedEveryMonth)
-    SELECT PR.Product_ID, 1
-    FROM PRODUCT PR
-    INNER JOIN PROPERTY P ON PR.Property_ID = P.Property_ID
-    WHERE P.Property_Name = @PropertyName;
-
-    -- Check each month for bookings
-    DECLARE @Month INT = 1;
-    WHILE @Month <= 12
-    BEGIN
-        -- Update status for rooms that were not booked in a given month
-        UPDATE #RoomsMonthlyBooked
-        SET RoomBookedEveryMonth = 0
-        FROM #RoomsMonthlyBooked RMB
-        WHERE RMB.Product_ID NOT IN (
-            SELECT RV.Product_ID
+    -- Select rooms that were booked at least once every month
+    SELECT 
+        PR.Product_ID, 
+        PR.Product_Description
+    FROM 
+        PRODUCT PR
+    INNER JOIN 
+        PROPERTY P ON PR.Property_ID = P.Property_ID
+    INNER JOIN 
+        PROPERTY_TYPE PT ON P.Property_Type_ID = PT.Property_Type_ID
+    WHERE 
+        PT.Property_Type_Name = @PropertyTypeName AND
+        12 = ( -- Check if the room was booked in all 12 months of the year
+            SELECT COUNT(DISTINCT MONTH(RV.Reservation_Date))
             FROM RESERVATIONS RV
-            WHERE RV.Product_ID = RMB.Product_ID 
-            AND YEAR(RV.Reservation_Date) = @Year 
-            AND MONTH(RV.Reservation_Date) = @Month
+            WHERE 
+                RV.Product_ID = PR.Product_ID AND 
+                YEAR(RV.Reservation_Date) = @Year
         )
-
-        SET @Month = @Month + 1;
-    END
-
-    -- Select rooms that were booked every month
-    SELECT PR.Product_ID, PR.Product_Description
-    FROM #RoomsMonthlyBooked RMB
-    JOIN PRODUCT PR ON RMB.Product_ID = PR.Product_ID
-    WHERE RMB.RoomBookedEveryMonth = 1;
-
-    -- Cleanup
-    DROP TABLE #RoomsMonthlyBooked;
 END
 
 
